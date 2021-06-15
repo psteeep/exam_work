5337708c1e69e1b82a069481620d21623d407246
psteeep <psteeep@users.noreply.github.com>
2021-06-15 09:41:36 +0300

    counting "a" at all

--- main.cpp
+++ main.cpp
@@ -8,8 +8,9 @@
 
 int main() {
 
-
+	int count_a_all = 0;
 	
+
 	char text[1024];
 	std::cout << "enter row: ";
 	
@@ -23,9 +24,15 @@ int main() {
 			
 			putchar(text[i]);  
 		}
+	
+		if (text[i] == 'a')
+			count_a_all++;
+
 	}
+	std::cout << std::endl;
 
-
+	std::cout << "Numb of a-sym at all = " << count_a_all << std::endl;
+	
 
 	return 0;
 }
