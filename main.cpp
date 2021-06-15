92c3f7d90f59f550097853ea1f6dded5180a4033
psteeep <psteeep@users.noreply.github.com>
2021-06-15 09:34:28 +0300

    add console input

--- main.cpp
+++ main.cpp
@@ -1,7 +1,29 @@
 #include <iostream>
+#include <string>
+#include <conio.h>
+//Ќаписати програму, €ка вводить з консол≥ слово та видаЇ, ск≥льки раз≥в
+//там зустр≥нетьс€ л≥тера УaФ ≥ ск≥льки раз≥в Ц л≥тера УaФ, за €кою Ќ≈ сл≥дуЇ
+//л≥тера УbФ(наприклад, слово abrakadabra Ц 5 ≥ 3).
 
 
 int main() {
+
+
+	
+	char text[1024];
+	std::cout << "enter row: ";
+	
+	for (int i = 0; i < 1024; i++)
+	{
+		text[i] = _getch(); 
+		if (text[i] == 13) // символ /n  
+			break;
+		else
+		{
+			
+			putchar(text[i]);  
+		}
+	}
 
 
 
